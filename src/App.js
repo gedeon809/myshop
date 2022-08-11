f2aa07864738e54d6e7bebc8c8714bff6dd827e8
Gedeon Kakona <gedeonkakona@gmail.com>
2022-08-11 21:29:11 +0200

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from 'react';
-import DirectoryComponent from './directory/DirectoryComponent';
+import HomeComponent from './routes/home/HomeComponent';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <DirectoryComponent categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomeComponent />} />
+    </Routes>
+  );
 };
 
 export default App;
